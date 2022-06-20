5fc60e1fa1df33c2dd9211020cf626691185dac7
Roman Valko <rvalko.dev@gmail.com>
2022-06-21 02:41:32 +0300

    all done

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 import '../css/styles.css';
 import { fetchCountries } from "./fetchCountries";
+import { buildListCountry } from "./builderCountry";
+import { buildCountryTempl } from "./builderCountry";
 import debounce from "lodash.debounce";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -13,31 +16,18 @@ const refs = {
 
 refs.inputValue.addEventListener('input', debounce(event => {
    const country = refs.inputValue.value.trim()
-   if (country.length >= 2) {
-      fetchCountries(country).then(country => {
-         console.log(country);
-         refs.countryList.innerHTML = buildListCountry(country)
-      })
-   } else {
+   if (country === '') {
       refs.countryList.innerHTML = ""
       return;
    }
+   fetchCountries(country).then(country => {
+      if(country.length > 1 && country.length <= 10){
+         refs.countryList.innerHTML = buildListCountry(country)
+      } else if (country.length == 1) {
+         refs.countryList.innerHTML = buildCountryTempl(country)
+      }
+      else if (country.length > 10) {
+         return Notify.info("Too many matches found. Please enter a more specific name.")
+      }
+      }).catch(error => Notify.failure("Oops, there is no country with that name"))
 },DEBOUNCE_DELAY))
-
-function buildListCountry(country) { 
-   let templatesCountry = "";
-   if(country.length > 1){
-   for (let key of country) {
-      templatesCountry += `<li class="styleForCoutryList"><img src="${key.flags.svg}" width
-="36px" height="30px">${key.name.official}</li>`
-   }
-      return templatesCountry
-   } else {
-      templatesCountry = `<li class="styleForCoutryList"><img src="${country[0].flags.svg}" width
-="36px" height="30px">${country[0].name.official}</li>
-<p>Capital: ${country[0].capital}</p>
-<p>Population: ${country[0].population}</p>
-<p>Languages: ${country[0].languages}</p>` // need think about, how to get values from languages
-   }
-   return templatesCountry
-}
